7bbcf3939915c9004d070e9e860b3949aabe4953
haydenlamprecht <hlamp13@gmail.com>
2026-04-15 18:39:39 -0400

    titled code

--- projects/MLB26/dataframe.py
+++ projects/MLB26/dataframe.py
@@ -15,57 +15,38 @@ df3 = pd.read_csv("C:\\Users\\hlamp\\OneDrive\\IndependentResearch\\hlamp13.gith
 #tot_hof
 df4 = pd.read_csv("C:\\Users\\hlamp\\OneDrive\\IndependentResearch\\hlamp13.github.io\\projects\\MLB26\\mlb_tot_hof.csv", encoding="latin1")
 
+#Merging the dataframes on the "Name" column.
 merged = df1.merge(df2, on = "Name", how = "left")
-
 merged_2 = merged.merge(df3, on = "Name", how = "left")
 merged_final = merged_2.merge(df4, on = "Name", how = "left")
-
+#Exporting the merged dataframe to CSV.
 merged_final.to_csv("C:\\Users\\hlamp\\OneDrive\\IndependentResearch\\hlamp13.github.io\\projects\\MLB26\\mlb_merged_final.csv", index=False, encoding="utf-8-sig")
 
-merged_final["clean name"] = merged_final["Name"].str.strip()
 
-merged_final["first"] = merged_final["clean name"].str.split(" ").str[0]
-merged_final["last"] = merged_final["clean name"].str.split(" ").str[-1]
-
-merged_final["first"] = merged_final["first"].str.replace(r"[^a-zA-Z]", "", regex=True).str.lower()
-merged_final["last"] = merged_final["last"].str.replace(r"[^a-zA-Z]", "", regex=True).str.lower()
-
-merged_final["playerID"] = (
-    merged_final["last"].str[:5].str.ljust(5, "x") +
-    merged_final["first"].str[:2].str.ljust(2, "x") +
-    "01"
-)
-
-merged_final.to_csv("C:\\Users\\hlamp\\OneDrive\\IndependentResearch\\hlamp13.github.io\\projects\\MLB26\\mlb_merged_final_1.csv", index=False, encoding="utf-8-sig")
-df5 = pd.read_csv("C:\\Users\\hlamp\\OneDrive\\IndependentResearch\\hlamp13.github.io\\projects\\MLB26\\mlb_merged_final_1.csv", encoding="utf-8-sig")
+#Merging existing dataframe with more career batting stats on PlayerID.
+df5 = pd.read_csv("C:\\Users\\hlamp\\OneDrive\\IndependentResearch\\hlamp13.github.io\\projects\\MLB26\\mlb_merged_final.csv", encoding="utf-8-sig")
 df6 = pd.read_csv("C:\\Users\\hlamp\\OneDrive\\IndependentResearch\\hlamp13.github.io\\projects\\MLB26\\careerbatting.csv", encoding="latin1")
-
 merged_playerid = df5.merge(df6, on = "playerID", how = "left")
-
+#Exporting again to CSV.
 merged_playerid.to_csv("C:\\Users\\hlamp\\OneDrive\\IndependentResearch\\hlamp13.github.io\\projects\\MLB26\\mlb_merged_final_2.csv", index=False, encoding="utf-8-sig")
 
+#Merging the existing dataframe with more career pitching stats on PlayerID.
 df7 = pd.read_csv("C:\\Users\\hlamp\\OneDrive\\IndependentResearch\\hlamp13.github.io\\projects\\MLB26\\mlb_merged_final_2.csv", encoding="utf-8-sig")
 df8 = pd.read_csv("C:\\Users\\hlamp\\OneDrive\\IndependentResearch\\hlamp13.github.io\\projects\\MLB26\\careerpitching.csv", encoding="latin1")
 merged_playerid_2 = df7.merge(df8, on = "playerID", how = "left")
+#Exporting again to CSV.
 merged_playerid_2.to_csv("C:\\Users\\hlamp\\OneDrive\\IndependentResearch\\hlamp13.github.io\\projects\\MLB26\\mlb_merged_final_3.csv", index=False, encoding="utf-8-sig")
 
+#Final corrections to the merged dataframe, mostly filling missing values.
 df9 = pd.read_csv("C:\\Users\\hlamp\\OneDrive\\IndependentResearch\\hlamp13.github.io\\projects\\MLB26\\playerid.csv", encoding="utf-8-sig")
-
 merged_playerid_3 = df9.merge(df6, on = "playerID", how = "left")
-
 merged_playerid_3.to_csv("C:\\Users\\hlamp\\OneDrive\\IndependentResearch\\hlamp13.github.io\\projects\\MLB26\\mlb_merged_missing_values.csv", index=False, encoding="utf-8-sig")
 
-###
 df10 = pd.read_csv("C:\\Users\\hlamp\\OneDrive\\IndependentResearch\\hlamp13.github.io\\projects\\MLB26\\mlb_merged_missing_values.csv", encoding="utf-8-sig")
-
 merged_missing_pitch = df10.merge(df8, on = "playerID", how = "left")
-
 merged_missing_pitch.to_csv("C:\\Users\\hlamp\\OneDrive\\IndependentResearch\\hlamp13.github.io\\projects\\MLB26\\mlb_merged_missing_values_pitch.csv", index=False, encoding="utf-8-sig")
 
 position = pd.read_csv("C:\\Users\\hlamp\\OneDrive\\IndependentResearch\\hlamp13.github.io\\projects\\MLB26\\careerposition.csv", encoding="latin1")
-
 final = pd.read_csv("C:\\Users\\hlamp\\OneDrive\\IndependentResearch\\hlamp13.github.io\\projects\\MLB26\\full_mlb_stats.csv", encoding="utf-8-sig")
-
 mlb_with_position = final.merge(position, on = "playerID", how = "left")
-
 mlb_with_position.to_csv("C:\\Users\\hlamp\\OneDrive\\IndependentResearch\\hlamp13.github.io\\projects\\MLB26\\full_mlb_stats_with_position.csv", index=False, encoding="utf-8-sig")
